548368489aacd4b00faba349b9646dcf3074967a
justinwhill <justinwhill@outlook.com>
2018-03-19 23:58:05 +0000

    user answers persist after submit

--- app/controllers/test_questions_controller.rb
+++ app/controllers/test_questions_controller.rb
@@ -3,11 +3,13 @@ class TestQuestionsController < ApplicationController
   def index
     @questions = Question.all
     @correctness = Hash[@questions.map {|question| [question.id, nil]}]
+    @answers = Hash[@questions.map {|question| [question.id, nil]}]
     if params[:answers] != nil
       params[:answers].each do |id, answer|
         id = id.to_i
         question = Question.find_by(:id => id)
-        @correctness[id.to_i] = (question.answer == answer)
+        @correctness[id] = (question.answer == answer)
+        @answers[id] = answer
       end
     end
   end
